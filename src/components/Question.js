8475812a40c3b80a791fbc7fad52d5dabe4518e5
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-08-19 02:07:08 +0700

    first commit

---
+++ src/components/Question.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import Option from './Option';
+
+class Question extends Component {
+    render() {
+        const {question, selectedOption, onOptionChange, onSubmit} = this.props;
+
+        return(
+            <div className="">
+                <h3>Question {question.id}</h3>
+                <h5 className="">{question.question}</h5>
+                <form onSubmit={onSubmit} className="mt-2 mb-2">
+                    <Option 
+                        options={question.options}
+                        selectedOption={selectedOption}
+                        onOptionChange={onOptionChange}
+                    />
+                <button type="submit" className="btn btn-primary mt-2">
+                    Submit
+                </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Question;
---
+++ src/components/QuestionBank.js
@@ -0,0 +1,35 @@
+
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital city of Japan?",
+        option: ["Jakarta", "Phnom Penh","Tokyo", "Washington"],
+        answer: "Tokyo",
+    },
+    {
+        id: 2,
+        question: "When was World War 2 began? ",
+        option: ["1990", "1939","1943", "1945"],
+        answer: "1945",
+    },
+    {
+        id: 3,
+        question: "Who started WW2 first?",
+        option: ["Hitler", "Narske", "Putin", "Zach"],
+        answer: "Hitler",
+    },
+    {
+        id: 4,
+        question: "which country is the #1 economy?",
+        option: ["Japan", "China","USA", "UK"],
+        answer: "USA",
+    },
+    {
+        id: 5,
+        question: "Who own Hong Kong in 1990s?",
+        option: ["British Colony", "China","Japan", "No one"],
+        answer: "British Colony",
+    },
+]
+
+export default qBank;
